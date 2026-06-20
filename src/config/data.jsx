c592df15073cb19de0a903a21c08578db1303c18
Nadson Costa <146302133+nadson-costa@users.noreply.github.com>
2026-06-20 13:57:24 -0300

    Update href link for Catálogo item

--- src/config/data.jsx
+++ src/config/data.jsx
@@ -12,7 +12,7 @@ export const MAIN_LINKS = [
     label: 'Catálogo',
     description: 'Confira nossas peças e coleções',
     icon: <ShoppingBag size={18} />,
-    href: 'https://drive.google.com/drive/folders/1ExmoOpjpQ3bBSxnd0tTZPeYf-5mBh4O4?usp=drive_link',
+    href: 'https://drive.google.com/drive/folders/1i7eCe4NleXFvwITU9sONYQMjGgTRBYIM?usp=sharing',
     type: 'external',
   },
   {
